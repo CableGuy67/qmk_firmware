e8f37ba72eb4750dc34334929879ca282fc119b7
mark <cableguy67@gmail.com>
2019-07-22 01:32:29 +0300

    cleanup of unused code

--- keyboards/planck/keymaps/markr/keymap.c
+++ keyboards/planck/keymaps/markr/keymap.c
@@ -25,7 +25,6 @@ enum planck_layers {
   _DVORAK,
   _LOWER,
   _RAISE,
-  _PLOVER,
   _ADJUST
 };
 
@@ -33,9 +32,7 @@ enum planck_keycodes {
   QWERTY = SAFE_RANGE,
   COLEMAK,
   DVORAK,
-  PLOVER,
   BACKLIT,
-  EXT_PLV
 };
 
 #define LOWER MO(_LOWER)
@@ -53,16 +50,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  |Brite | GUI  |    Space    |    Space    | Left |  Up  | Down |Right |
+ * | Ctrl | Alt  |Brite | GUI  |    Space    |    Space    | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_2x2u(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     CAPS_SH, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENT_SH,
-    KC_LCTL, KC_LALT, BACKLIT, KC_LGUI, SP_LOWR, SP_RAISE, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LALT, BACKLIT, KC_LGUI, SP_LOWR, SP_RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 
 
@@ -105,9 +102,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * | Reset|      |      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|Plover|      |
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|Colemk|Dvorak|      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -115,18 +112,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_2x2u(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,
+    RESET, _______,  DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  _______,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______,     _______     ,     _______     , _______, _______,  _______, _______
 )
 
 };
 
-#ifdef AUDIO_ENABLE
-  float plover_song[][2]     = SONG(PLOVER_SOUND);
-  float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
-#endif
+// #ifdef AUDIO_ENABLE
+//   float plover_song[][2]     = SONG(PLOVER_SOUND);
+//   float plover_gb_song[][2]  = SONG(PLOVER_GOODBYE_SOUND);
+// #endif
 
 uint32_t layer_state_set_user(uint32_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
@@ -170,34 +167,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case PLOVER:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          stop_all_notes();
-          PLAY_SONG(plover_song);
-        #endif
-        layer_off(_RAISE);
-        layer_off(_LOWER);
-        layer_off(_ADJUST);
-        layer_on(_PLOVER);
-        if (!eeconfig_is_enabled()) {
-            eeconfig_init();
-        }
-        keymap_config.raw = eeconfig_read_keymap();
-        keymap_config.nkro = 1;
-        eeconfig_update_keymap(keymap_config.raw);
-      }
-      return false;
-      break;
-    case EXT_PLV:
-      if (record->event.pressed) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(plover_gb_song);
-        #endif
-        layer_off(_PLOVER);
-      }
-      return false;
-      break;
+//     case PLOVER:
+//       if (record->event.pressed) {
+//         #ifdef AUDIO_ENABLE
+//           stop_all_notes();
+//           PLAY_SONG(plover_song);
+//         #endif
+//         layer_off(_RAISE);
+//         layer_off(_LOWER);
+//         layer_off(_ADJUST);
+//         layer_on(_PLOVER);
+//         if (!eeconfig_is_enabled()) {
+//             eeconfig_init();
+//         }
+//         keymap_config.raw = eeconfig_read_keymap();
+//         keymap_config.nkro = 1;
+//         eeconfig_update_keymap(keymap_config.raw);
+//       }
+//       return false;
+//       break;
+//     case EXT_PLV:
+//       if (record->event.pressed) {
+//         #ifdef AUDIO_ENABLE
+//           PLAY_SONG(plover_gb_song);
+//         #endif
+//         layer_off(_PLOVER);
+//       }
+//       return false;
+//       break;
   }
   return true;
 }
@@ -248,14 +245,14 @@ void dip_update(uint8_t index, bool active) {
   switch (index) {
     case 0:
       if (active) {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(plover_song);
-        #endif
+//         #ifdef AUDIO_ENABLE
+//           PLAY_SONG(plover_song);
+//         #endif
         layer_on(_ADJUST);
       } else {
-        #ifdef AUDIO_ENABLE
-          PLAY_SONG(plover_gb_song);
-        #endif
+//         #ifdef AUDIO_ENABLE
+//           PLAY_SONG(plover_gb_song);
+//         #endif
         layer_off(_ADJUST);
       }
       break;
