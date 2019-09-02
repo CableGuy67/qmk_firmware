02951ff1ddd72a3545191bfb326219af353e70ba
mark <cableguy67@gmail.com>
2019-09-03 02:38:14 +0300

    loose greek and well,yeah

--- keyboards/planck/keymaps/markr/keymap.c
+++ keyboards/planck/keymaps/markr/keymap.c
@@ -21,23 +21,23 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
-  _GREEK,
-  _LOWER,
-  _RAISE,
+  _UTIL,
+  _SYMB,
+  _NUMS,
   _ADJUST
 };
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  GREEK
+  UTIL
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define SYMB MO(_SYMB)
+#define NUMS MO(_NUMS)
 #define CAPS_SH LSFT_T(KC_CAPSLOCK)
 #define ENT_SH RSFT_T(KC_ENT)
-#define SP_LOWR LT(LOWER, KC_SPC)
-#define SP_RAISE LT(RAISE, KC_SPC)
+#define SP_SYMB LT(SYMB, KC_SPC)
+#define SP_NUMS LT(NUMS, KC_SPC)
 
 // Greek key codes
 // uppercase
@@ -120,37 +120,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Enter|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  |Greek | GUI  |    Space    |    Space    | Left | Down |  Up  |Right |
+ * | Ctrl | Alt  | Util | GUI  |    Space    |    Space    | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_2x2u(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     CAPS_SH, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_SLSH, ENT_SH,
-    KC_LCTL, KC_LALT, GREEK, KC_LGUI, SP_LOWR, SP_RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    KC_LCTL, KC_LALT, UTIL, KC_LGUI, SP_SYMB, SP_NUMS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
 ),
 
 
-/* Greek
+/* Util
  * ,-----------------------------------------------------------------------------------.
- * | Esc  |   ;  |   ς  |   Ε  |   Ρ  |   Τ  |   Υ  |   Θ  |   Ι  |   Ο  |   Π  | Bksp |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   Α  |   Σ  |   Δ  |   Φ  |   Γ  |   Η  |   Ξ  |   Κ  |   Λ  |   ;  |  "   |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Ζ  |   Χ  |   Ψ  |   Ω  |   Β  |   Ν  |   Μ  |   ,  |   .  |   /  | Enter|
+ * |      |      |      |      |      |      |      |      |      |      |  Up  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Alt  |Qwerty| GUI  |    Space    |    Space    | Left | Down |  Up  |Right |
+ * |      |      |Qwerty|      |             |             |      | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_GREEK] = LAYOUT_planck_2x2u(
-    KC_ESC,  KC_Q,    UC(FSIGM),    UC(LEPSI),    UC(LRHO),     UC(LTAU),    UC(LUPSI),     UC(LTHET),    UC(LIOTA),    UC(LOMIC),    UC(LPI),    KC_BSPC,
-    KC_TAB,  UC(LALPH),    UC(LSIGM),    UC(LDELT),    UC(LPHI),     UC(LGAMM),    UC(LETA),     UC(LXI),    UC(LKAPP),    UC(LLAMB),    KC_SCLN, KC_QUOT,
-    CAPS_SH, UC(LZETA),    UC(LCHI),    UC(LPSI),    UC(LOMEG),     UC(LBETA),    UC(LNU),     UC(LMU),    KC_COMM, KC_DOT,  KC_SLSH, ENT_SH,
-    KC_LCTL, KC_LALT, QWERTY, KC_LGUI, SP_LOWR, SP_RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+[_UTIL] = LAYOUT_planck_2x2u(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,    _______,
+	_______, _______, QWERTY, _______,     _______     ,     _______     , _______, KC_LEFT, KC_DOWN,    KC_RGHT
 ),
 
 
-/* Lower
+/* Symbol
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -161,7 +161,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |             |             | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_planck_2x2u(
+[_SYMB] = LAYOUT_planck_2x2u(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
@@ -170,24 +170,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |             |             | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_planck_2x2u(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+[_NUMS] = LAYOUT_planck_2x2u(
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+    _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
     _______, _______, _______, _______,     _______     ,     _______     , KC_MNXT, KC_VOLD,  KC_VOLU, KC_MPLY
 ),
 
 
-/* Adjust (Lower + Raise)
+/* Adjust (Symbol + Raise)
  * ,-----------------------------------------------------------------------------------.
  * | Reset|      |      |      |      |      |      |      |      |      |      |  Del |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -208,7 +208,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 uint32_t layer_state_set_user(uint32_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _SYMB, _NUMS, _ADJUST);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -219,9 +219,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case GREEK:
+    case UTIL:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_GREEK);
+        set_single_persistent_default_layer(_UTIL);
       }
       return false;
       break;
@@ -229,101 +229,114 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-bool muse_mode = false;
-uint8_t last_muse_note = 0;
-uint16_t muse_counter = 0;
-uint8_t muse_offset = 70;
-uint16_t muse_tempo = 50;
-
-void encoder_update(bool clockwise) {
-  if (muse_mode) {
-    if (IS_LAYER_ON(_RAISE)) {
-      if (clockwise) {
-        muse_offset++;
-      } else {
-        muse_offset--;
-      }
-    } else {
-      if (clockwise) {
-        muse_tempo+=1;
-      } else {
-        muse_tempo-=1;
-      }
-    }
-  } else {
-    if (clockwise) {
-      #ifdef MOUSEKEY_ENABLE
-        register_code(KC_MS_WH_DOWN);
-        unregister_code(KC_MS_WH_DOWN);
-      #else
-        register_code(KC_PGDN);
-        unregister_code(KC_PGDN);
-      #endif
-    } else {
-      #ifdef MOUSEKEY_ENABLE
-        register_code(KC_MS_WH_UP);
-        unregister_code(KC_MS_WH_UP);
-      #else
-        register_code(KC_PGUP);
-        unregister_code(KC_PGUP);
-      #endif
-    }
-  }
-}
-
-void dip_update(uint8_t index, bool active) {
-  switch (index) {
-    case 0:
-      if (active) {
+// bool muse_mode = false;
+// uint8_t last_muse_note = 0;
+// uint16_t muse_counter = 0;
+// uint8_t muse_offset = 70;
+// uint16_t muse_tempo = 50;
+// 
+// void encoder_update(bool clockwise) {
+//   if (muse_mode) {
+//     if (IS_LAYER_ON(_NUMS)) {
+//       if (clockwise) {
+//         muse_offset++;
+//       } else {
+//         muse_offset--;
+//       }
+//     } else {
+//       if (clockwise) {
+//         muse_tempo+=1;
+//       } else {
+//         muse_tempo-=1;
+//       }
+//     }
+//   } else {
+//     if (clockwise) {
+//       #ifdef MOUSEKEY_ENABLE
+//         register_code(KC_MS_WH_DOWN);
+//         unregister_code(KC_MS_WH_DOWN);
+//       #else
+//         register_code(KC_PGDN);
+//         unregister_code(KC_PGDN);
+//       #endif
+//     } else {
+//       #ifdef MOUSEKEY_ENABLE
+//         register_code(KC_MS_WH_UP);
+//         unregister_code(KC_MS_WH_UP);
+//       #else
+//         register_code(KC_PGUP);
+//         unregister_code(KC_PGUP);
+//       #endif
+//     }
+//   }
+// }
+// 
+// void dip_update(uint8_t index, bool active) {
+//   switch (index) {
+//     case 0:
+//       if (active) {
+// //         #ifdef AUDIO_ENABLE
+// //           PLAY_SONG(plover_song);
+// //         #endif
+//         layer_on(_ADJUST);
+//       } else {
+// //         #ifdef AUDIO_ENABLE
+// //           PLAY_SONG(plover_gb_song);
+// //         #endif
+//         layer_off(_ADJUST);
+//       }
+//       break;
+//     case 1:
+//       if (active) {
+//         muse_mode = true;
+//       } else {
+//         muse_mode = false;
 //         #ifdef AUDIO_ENABLE
-//           PLAY_SONG(plover_song);
+//           stop_all_notes();
 //         #endif
-        layer_on(_ADJUST);
-      } else {
-//         #ifdef AUDIO_ENABLE
-//           PLAY_SONG(plover_gb_song);
-//         #endif
-        layer_off(_ADJUST);
-      }
-      break;
-    case 1:
-      if (active) {
-        muse_mode = true;
-      } else {
-        muse_mode = false;
-        #ifdef AUDIO_ENABLE
-          stop_all_notes();
-        #endif
-      }
-   }
-}
+//       }
+//    }
+// }
+// 
+// void matrix_scan_user(void) {
+//   #ifdef AUDIO_ENABLE
+//     if (muse_mode) {
+//       if (muse_counter == 0) {
+//         uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+//         if (muse_note != last_muse_note) {
+//           stop_note(compute_freq_for_midi_note(last_muse_note));
+//           play_note(compute_freq_for_midi_note(muse_note), 0xF);
+//           last_muse_note = muse_note;
+//         }
+//       }
+//       muse_counter = (muse_counter + 1) % muse_tempo;
+//     }
+//   #endif
+// }
+// 
+// bool music_mask_user(uint16_t keycode) {
+//   switch (keycode) {
+//     case NUMS:
+//     case SYMB:
+//       return false;
+//     default:
+//       return true;
+//   }
+// }
 
-void matrix_scan_user(void) {
-  #ifdef AUDIO_ENABLE
-    if (muse_mode) {
-      if (muse_counter == 0) {
-        uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-        if (muse_note != last_muse_note) {
-          stop_note(compute_freq_for_midi_note(last_muse_note));
-          play_note(compute_freq_for_midi_note(muse_note), 0xF);
-          last_muse_note = muse_note;
-        }
-      }
-      muse_counter = (muse_counter + 1) % muse_tempo;
-    }
-  #endif
-}
+// This just doesn't work correctly...
+// It works the same way without the following as with.
 
-bool music_mask_user(uint16_t keycode) {
-  switch (keycode) {
-    case RAISE:
-    case LOWER:
-      return false;
-    default:
-      return true;
-  }
-}
-
-void matrix_init_user(void) {
-    set_unicode_input_mode(UC_LNX); // REPLACE UC_XXXX with the Unicode Input Mode for your OS. See table below.
-}
+// #ifdef UNICODE_KEY_LNX
+//   #undef UNICODE_KEY_LNX
+// #endif
+// // default is LCTL(LSFT(KC_U))
+// #define UNICODE_KEY_LNX LCTL(LSFT(KC_E))
+// void matrix_init_user(void) {
+//   // UC_WIN, UC_OSX or UC_LNX to se Unicode Input Mode of the OS
+//   // This sort of works for my Debian box but not with all apps. Kate and Dolphin (KDE) don't not work
+//   // correctly but it does in Gnome mostly (gedit, terminal). When it doesn't work it just outputs the
+//   // raw codes without composing them into a character.
+//   
+//     set_unicode_input_mode(UC_LNX); // REPLACE UC_XXXX with the Unicode Input Mode for your OS. See table below.
+// }
